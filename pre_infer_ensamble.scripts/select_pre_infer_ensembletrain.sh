bae523eda6bd7e88fc75632c7ee8c023d47cd9a9
LuChungYing <35101191+LuChungYing@users.noreply.github.com>
2019-10-27 23:02:09 +0800

    Add files via upload

--- pre_infer_ensamble.scripts/select_pre_infer_ensembletrain.sh
+++ pre_infer_ensamble.scripts/select_pre_infer_ensembletrain.sh
@@ -3,8 +3,9 @@ model=$1
 
 output_dir="/home/lulu/yt8m/models/model_predictions/${model}/"
 
-python infer_pre_ensemble_test2.py \
+python infer_pre_ensemble_test_train.py \
         --output_dir="$output_dir" \
+        --output_file='/home/lulu/yt8m/output/dbof.csv'\
         --train_dir /home/lulu/yt8m/models/frame/${model} \
         --input_data_pattern="/home/lulu/yt8m/3/frame/validate/*.tfrecord" \
         --frame_features=True \
@@ -12,4 +13,4 @@ python infer_pre_ensemble_test2.py \
         --feature_sizes="1024,128" \
         --model=${model} \
         --batch_size=32 \
-        --segment_labels
+        --segment_labels \
